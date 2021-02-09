b2a6fe1cf175c45754d049c30e69c0744da639a0
Nelson-Gon <gonzabato@hotmail.com>
2021-02-09 10:55:47 +0800

    Update comments

--- cellseg/model.py
+++ cellseg/model.py
@@ -1,7 +1,6 @@
 import torch.nn as nn
 
 
-
 class CellNet(nn.Module):
     def __init__(self, input_shape=32, channels=1):
         super(CellNet, self).__init__()
@@ -15,7 +14,7 @@ class CellNet(nn.Module):
         # Define length of flattened layer
         # Calculating input of FCN w' = (w - f + 2p)/s + 1 where w is from inputs eg 1 * 16 * 16
         # For a 4 by 4 kernel, after halving with max pooling and same padding --> [batch_size, 256, 254] --> 127
-        self.fc1 = nn.Linear(32*15*15, 64)  # Dense layer with output 512
+        self.fc1 = nn.Linear(32 * 15 * 15, 64)  # Dense layer with output 512
         self.drop = nn.Dropout(0.5)
 
         # input from previous layer
@@ -23,14 +22,10 @@ class CellNet(nn.Module):
         self.act = nn.ReLU()
 
     def forward(self, x):
-        x = self.act(self.conv1(x))  # [batch_size, 256, 256, 254]
-        x = self.pool(x)  # [batch_size, 256, 127, 127]
-        x = x.view(x.size(0), -1)  # [batch_size, 256*127*127=4129024]
-        x = self.act(self.fc1(x))  # [batch_size, 512]
+        x = self.act(self.conv1(x))  # [batch_size, 32, 32, 30]
+        x = self.pool(x)  # [batch_size, 32, 15, 15]
+        x = x.view(x.size(0), -1)  # [batch_size, 32*15*15=7200] #number of input features
+        x = self.act(self.fc1(x))  # [batch_size, 64]
         x = self.drop(x)
-        x = self.out(x)  # [batch_size, 3]
+        x = self.out(x)  # [batch_size, number_predictions]
         return x
-
-
-
-
