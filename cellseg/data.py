b73ced80371122d2d31312e9b770376ef57b4773
Nelson-Gon <gonzabato@hotmail.com>
2021-02-20 21:01:59 +0800

    Fix issues with data transforms, todo: working training

--- cellseg/data.py
+++ cellseg/data.py
@@ -1,3 +1,4 @@
+# DataProcessor
 from torch.utils.data import Dataset
 import torchvision.transforms.functional as TF
 import torchvision.transforms as transforms
@@ -11,29 +12,39 @@ from cv2 import convertScaleAbs
 
 
 class DataProcessor(Dataset):
-    def __init__(self, train_image_dir, train_mask_dir, target_size=(512, 512), image_suffix="tif"):
+    def __init__(self, image_dir, dir_type="image", target_size=(512, 512), image_suffix="tif"):
         """
 
-        :param train_image_dir: Path to train images directory
-        :param train_mask_dir: Path to train masks (labels) directory
-        :param target_size: Target size to resize to. Defaults to (512, 512). This was found to be the best for data
-        preservation.
+        :param image_dir: Path to images directory. Either images or masks dir
+        :param dir_type: One of "image" or "mask" Defaults to image
+        :param target_size: Target size to resize to. Defaults to (512, 512).
+        This was found to be the best for data preservation.
         :param image_suffix: Image suffix of the images. Defaults to "tif"
 
-        :return An object of class DataProcessor ---> super class Dataset
+        :return An object of class DataProcessor ---> torch.utils.data.Dataset
 
         """
-        self.train_image_dir = train_image_dir
+        self.image_dir = image_dir
         self.image_suffix = image_suffix
-        self.train_mask_dir = train_mask_dir
+        self.dir_type = dir_type
         self.target_size = target_size
-        self.train_image_list = sorted(glob.glob(self.train_image_dir + "/*." + self.image_suffix))
-        self.train_mask_list = sorted(glob.glob(self.train_mask_dir + "/*." + self.image_suffix))
+        self.image_list = sorted(glob.glob(self.image_dir + "/*." + self.image_suffix))
 
     def __len__(self):
-        return len(self.train_mask_list)
+        return len(self.image_list)
 
-    def transform(self, image, mask):
+    def to_uint8(self, image):
+        if self.dir_type == "mask":
+            uint8_img = convertScaleAbs(image, alpha=255.0 / 65535.0)
+        else:
+            uint8_img = convertScaleAbs(image)
+
+        return uint8_img
+
+
+
+
+    def transform(self, image):
         """
 
         :param image: Stacked image eg (71, 1200, 1200)
@@ -43,63 +54,54 @@ class DataProcessor(Dataset):
         """
         resize_image = transforms.Resize(size=self.target_size)
         image = resize_image(image)
-        mask = resize_image(mask)
+        # mask = resize_image(mask)
         # https://discuss.pytorch.org/t/torchvision-transfors-how-to-perform-identical-transform-on-both-image-and-target/10606/7
 
-    
         # Random cropping
 
         i, j, h, w = transforms.RandomCrop.get_params(
             image, output_size=self.target_size)
         image = TF.crop(image, i, j, h, w)
-        mask = TF.crop(mask, i, j, h, w)
+        # mask = TF.crop(mask, i, j, h, w)
 
         # Random horizontal flipping
         if random.random() > 0.5:
             image = TF.hflip(image)
-            mask = TF.hflip(mask)
+            # mask = TF.hflip(mask)
 
         # Random vertical flipping
         if random.random() > 0.5:
             image = TF.vflip(image)
-            mask = TF.vflip(mask)
+            # mask = TF.vflip(mask)
 
         # Transform to tensor
 
         image = TF.to_tensor(image)
-        mask = TF.to_tensor(mask)
-
-
-
-        return image, mask
+        # mask = TF.to_tensor(mask)
+        return image
 
     def __getitem__(self, img_index):
         """
 
         :param img_index: Index of the image
-        :return: A dictionary containing an image, mask, and the index of that item.
+        :return: Image
         """
+
         if torch.is_tensor(img_index):
             img_index = img_index.tolist()
         if self.image_suffix == "tif":
-            final_train_image = imread(self.train_image_list[img_index], plugin="pil")
-            final_train_mask = imread(self.train_mask_list[img_index], plugin="pil")
+            final_images = imread(self.image_list[img_index], plugin="pil")
+
         else:
-            # WIP: Using uint16 with PIL leads to loss of information
-            final_train_image = imread(self.train_image_list[img_index])
-            final_train_mask = imread(self.train_mask_list[img_index])
-        # Convert images to PIL/Tensor
-        # List comprehension since we have image stacks.
-        # uint16 doesn't work with resizing --> convert to uint8 but preserve information
-        # Given the data, masks do not work well with alpha scaling
-        final_mask_train = [Image.fromarray(convertScaleAbs(x)) for x in final_train_mask]
-        final_image_train = [Image.fromarray(convertScaleAbs(x, alpha=255.0/65535.0)) for x in final_train_image]
-        final_image_list = []
-        final_mask_list = []
+            final_images = imread(self.image_list[img_index])
 
-        for image, mask in zip(final_image_train, final_mask_train):
-            image, mask = self.transform(image, mask)
-            final_image_list.append(image)
-            final_mask_list.append(mask)
+        # # Convert images to PIL/Tensor
+        # # List comprehension since we have image stacks.
+        # # uint16 doesn't work with resizing --> convert to uint8 but preserve information
+        # # Given the data, masks do not work well with alpha scaling
 
-        return {"image": final_image_list, "mask": final_mask_list, "index": img_index}
+
+        return list(map(lambda x: self.transform(Image.fromarray(self.to_uint8(x))), final_images))
+
+
+
--- cellseg/training.py
+++ cellseg/training.py
@@ -1,23 +1,35 @@
-# TODO: For each frame with an image stack, track cells, and quantify them
 from torch.optim import Adam, RMSprop, SGD
 from torch import nn
+from torch.utils.data import DataLoader
 
-# Define Loss function --> BCE
+train_images = DataProcessor("D:\\train_images\\images", target_size=(32, 32))
+train_labels = DataProcessor("D:\\train_images\masks", dir_type="masks", target_size = (32, 32))
+train_labels[0][0].dtype
+train_images[0][0].dtype
+# Train with each stacked image
+train_x = DataLoader(train_images[0], batch_size=2)
+train_y = DataLoader(train_labels[0], batch_size=2)
 
-loss_function = nn.BCELoss()
-# Use Adam as optimizer
-optimizer = Adam(model.parameters(), lr=0.0001)
 
-# Track loss
-losses_per_batch =[]
+unsq= train_images[0][0].unsqueeze(0)
 
-for epoch in range(3):
-    get_iteration = iter(DataProcessor)
-    for batch in get_iteration:
-        optimizer.zero_grad()
+# Define model
+model = CellNet()
+# Define Criterion
+criterion = nn.BCELoss()
+# Define optimizer
+optimizer = Adam(model.parameters(), lr=0.001)
 
-        output = model(batch["image"])
-        loss = loss_function(output, batch["masks"])
+losses_per_batch = []
+
+for epoch in range(1):
+    for index, (img, msk) in enumerate(zip(train_images[0], train_labels[0])):
+        img = img.unsqueeze(0)
+        msk = msk.unsqueeze(0)
+        output = model(img)
+        loss = criterion(output, msk)
         losses_per_batch.append(loss)
         loss.backward()
         optimizer.step()
+        print("Done*******************************")
+
