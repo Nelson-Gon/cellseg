a5624daf5e06c74f2229e71da69bb53dcbd7ddee
Nelson-Gon <gonzabato@hotmail.com>
2021-03-20 23:51:36 +0800

    Drop templates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ source_formats = [".rst", ".md"]
 # ones.
 extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -54,5 +54,5 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+
 
