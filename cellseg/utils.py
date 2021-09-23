2cf6f7fc6809570b821fc4731b7d9e0f494c4941
Nelson-Gon <gonzabato@hotmail.com>
2021-09-23 12:37:13 +0300

    Fix tests

--- cellseg/utils.py
+++ cellseg/utils.py
@@ -16,14 +16,16 @@ def show_images(dataset_object, number=None, fig_size=(20, 20), target="image"):
 
     if not isinstance(dataset_object, DataProcessor):
         raise TypeError(f"Expected an object of class DataProcessor not {type(dataset_object).__name__}")
-
-    if isinstance(number, int) and number <= dataset_object.__len__:
+    img_len = dataset_object.__len__
+    if 0 < number < img_len and isinstance(number, int):
         plt_figure = plt.figure(figsize=fig_size)
-        assert 0 < number < dataset_object.__len__, f"number should be a non-zero int and less than or " \
-                                                    f"equal to {dataset_object.__len__} not {number} "
         num_cols = number / 2 if number % 2 == 0 else number / 3
         for img_index in range(number):
             subplt = plt_figure.add_subplot(2, int(ceil(num_cols)), img_index + 1)
             # Current shape is channels first, drop first dimension (channel) for viewing
             subplt.imshow(dataset_object[img_index][target][0, :, :], cmap="gray")
+    else:
+        raise ValueError(f"number should be a non-zero int and less than or equal to {img_len} not {number}")
+
+
 
--- tests/tests.py
+++ tests/tests.py
@@ -3,6 +3,7 @@ import os
 from cellseg.data import DataProcessor
 from cellseg.utils import show_images
 from unittest import mock
+
 # Create Paths to test files -- data here
 
 # Get directory name one level up
@@ -19,28 +20,28 @@ test_lengths = os.path.join(base_dir, "data/validation/dummy_for_tests")
 
 # Wrong paths
 
-not_valid_path  = os.path.join(base_dir, "not_valid")
+not_valid_path = os.path.join(base_dir, "not_valid")
 
 # Create a DataProcessor object from these paths
 
 dataset_object = DataProcessor(image_dir=test_data_images, label_dir=test_data_labels, target_size=(512, 512),
                                image_suffix="tif")
 
-class cellsegTests(unittest.TestCase):
+
+class TestClass(unittest.TestCase):
 
     def test_instance_creation(self):
-
         self.assertIsInstance(dataset_object, DataProcessor)
 
     def test_DataProcessor_class(self):
         with self.assertRaises(NotADirectoryError) as err:
-            DataProcessor(image_dir=not_valid_path,label_dir=test_data_labels)
+            DataProcessor(image_dir=not_valid_path, label_dir=test_data_labels)
         self.assertEqual(str(err.exception), "One or both directories do not exist.")
 
         # Ensure that we error on unequal lengths
 
         with self.assertRaises(ValueError) as err:
-            DataProcessor(image_dir = test_data_images, label_dir= test_lengths,
+            DataProcessor(image_dir=test_data_images, label_dir=test_lengths,
                           image_suffix="tif")
         self.assertEqual(str(err.exception), "Found 10 images but 1 label.")
 
@@ -52,15 +53,15 @@ class cellsegTests(unittest.TestCase):
         with self.assertRaises(TypeError) as err:
             DataProcessor(test_data_images,
                           test_data_labels,
-                                  image_suffix="tif",
-                                  target_size = [256, 256])
+                          image_suffix="tif",
+                          target_size=[256, 256])
         self.assertEqual(str(err.exception), "Target size should be a tuple not list")
 
     def test_transforms(self):
         # Ensure that transforms work as expected
         transforms_object = DataProcessor(test_data_images,
                                           test_data_labels,
-                                          target_size = (256, 256))
+                                          target_size=(256, 256))
 
         # Ensure that at each index we get images, labels, and an index
 
@@ -80,26 +81,25 @@ class cellsegTests(unittest.TestCase):
 
     def test_utils(self):
         with self.assertRaises(ValueError) as err:
-            show_images(dataset_object, number = 42)
-        self.assertEqual(str(err.exception),"number should be an int and less than or equal to 10 not 42")
+            show_images(dataset_object, number=42)
+        self.assertEqual(str(err.exception), "number should be a non-zero int and less than or equal to 10 not 42")
         # Ensure that we only have image and mask in target
         # There must be a way to not repeat these steps and test at once
         with self.assertRaises(ValueError) as err:
-            show_images(dataset_object, number = 8, target="gibberish")
+            show_images(dataset_object, number=8, target="gibberish")
 
         self.assertEqual(str(err.exception), "Target should be one of image or mask not gibberish")
 
         # Check that we provide the right data type
         with self.assertRaises(TypeError) as err:
-            show_images("not a DataProcessor object", number = 4)
+            show_images("not a DataProcessor object", number=4)
 
-        self.assertEqual(str(err.exception),"Expected an object of class DataProcessor not str")
-
+        self.assertEqual(str(err.exception), "Expected an object of class DataProcessor not str")
 
     # Mock plots
     @mock.patch("cellseg.utils.plt")
     def test_plots(self, mock_plt):
-        show_images(dataset_object, number = 4)
+        show_images(dataset_object, number=4)
         mock_plt.cmap.called_once_with("gray")
         mock_plt.figure.called_once()
         # mock_plt.figure().add_subplot.assert_called_once()
